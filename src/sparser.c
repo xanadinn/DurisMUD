2bc41731ca69cb212ab7beaaf42a7bb7013fab4b
Venthix <kyse1@verizon.net>
2010-07-27 22:21:40 +0000

    Fixing issue with Imm's not being able to cast spells.

--- src/sparser.c
+++ src/sparser.c
@@ -1529,7 +1529,7 @@ bool parse_spell(P_char ch, char *argument,
       return FALSE;
     }
   }
-  else if (circle != -1)
+  else if ((circle != -1) && !IS_TRUSTED(ch))
   {
     send_to_char("&+RYou don't have that spell memorized.\n", ch);
     return FALSE;
