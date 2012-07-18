4f5cb16ff46b4b533ccc41c9633dfedd970607e7
Lohrr <Lohrr2k@yahoo.com>
2012-07-18 16:12:48 +0000

    Added a message to guides when a newbie logs in.

--- src/nanny.c
+++ src/nanny.c
@@ -2070,6 +2070,7 @@ void enter_game(P_desc d)
   int      mana_g;
   char     Gbuf1[MAX_STRING_LENGTH];
   P_char   ch = d->character;
+  P_desc   i;
   
   if (GET_LEVEL(ch))
   {
@@ -2327,10 +2328,32 @@ void enter_game(P_desc d)
     act("$n has entered the game.", TRUE, ch, 0, 0, TO_ROOM);
 
   // inform gods that a newbie has entered the game
-  if( IS_NEWBIE(ch)) {
+  if( IS_NEWBIE(ch))
+  {
     statuslog(ch->player.level, "&+GNEWBIE %s HAS ENTERED THE GAME! Help him out :) ", GET_NAME(ch));
+    // Message to guides.
+    sprintf( Gbuf1, "&+GNEWBIE %s HAS ENTERED THE GAME! Help him out :)\n", 
+      GET_NAME(ch));
+    for (i = descriptor_list; i; i = i->next)
+    {
+      if(i->connected)
+        continue;
+ 
+      if( opposite_racewar( ch, i->character ) )
+        continue;
+      if(!IS_SET(i->character->specials.act2, PLR2_NCHAT))
+        continue;
+      if(!IS_SET(PLR2_FLAGS(i->character), PLR2_NEWBIE_GUIDE))
+        continue;
+      if(IS_DISGUISE_PC(i->character) ||
+        IS_DISGUISE_ILLUSION(i->character) ||
+        IS_DISGUISE_SHAPE(i->character))
+        continue;
+ 
+      send_to_char(Gbuf1, i->character, LOG_PRIVATE);
+    }
   }
-  
+
   if (!GET_LEVEL(ch))
   {
     do_start(ch, 0);
