bdbdb6752b7c41ad141f886c24d00dad9434c06f
Kitana <todd@frogtechnical.com>
2013-10-07 18:25:41 +0000

    automated compile

--- src/ships/ship_base.c
+++ src/ships/ship_base.c
@@ -2250,7 +2250,7 @@ int read_ships()
 //--------------------------------------------------------------------
 void update_shipfrags()
 {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 200; i++)
     {
         int max = 0;
         shipfrags[i].ship = 0;
