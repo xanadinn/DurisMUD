5dbe5ac5faea8b59dce41c357a4f40bad42937d1
Lohrr <Lohrr2k@yahoo.com>
2014-06-10 01:46:43 +0000

    Fixed disruptive blow.  It was using the caster as the merc and the merc as the caster.

--- src/sparser.c
+++ src/sparser.c
@@ -1968,7 +1968,6 @@ void do_will(P_char ch, char *argument, int cmd)
 
 bool check_disruptive_blow(P_char ch)
 {
-  P_char tch;
   struct damage_messages messages = {
     "You lunge, slamming your fist into $N larynx.",
     "$n lunges, slamming $s fist into your throat.",
@@ -1985,55 +1984,37 @@ bool check_disruptive_blow(P_char ch)
     "$n lunges, slamming $s fist into your throat. That did it.",
     "$n lunges, slamming $s fist into $N larynx. $E's dead."
   };
-  int skl = GET_CHAR_SKILL(ch, SKILL_DISRUPTIVE_BLOW);
+  int skl;
   int success;
+  P_char tch;
 
-  if(!(ch) ||
-     !ch->specials.fighting ||
-     IS_IMMOBILE(ch) ||
-     !AWAKE(ch) ||
-     IS_STUNNED(ch) ||
-     !IS_HUMANOID(ch))
-  {
-    return false;
-  }
-  
+  if( !(ch) || !ch->specials.fighting || IS_IMMOBILE(ch)
+    || !AWAKE(ch) || IS_STUNNED(ch) || !IS_HUMANOID(ch))
+    return FALSE;
+
+  // tch is the merc in this situation.
   for (tch = world[ch->in_room].people; tch; tch = tch->next_in_room)
   {
     if (tch == ch)
-    {
       continue;
-    }
-    
-    if(IS_IMMATERIAL(tch) ||
-       IS_GREATER_RACE(tch) ||
-       IS_ELITE(tch) ||
-       !IS_HUMANOID(tch))
-    {
+
+    if(IS_IMMATERIAL(tch) || !IS_HUMANOID(tch))
       continue;
-    }
-    
+
     if (GET_POS(tch) != POS_STANDING)
-    {
       continue;
-    }
 
-    if(tch->specials.fighting == ch)
-    {
+    // Merc must be targeting caster.
+    if(tch->specials.fighting != ch)
       continue;
-    }
-    
-    if(ch->specials.fighting != tch)
-    {
-      continue;
-    }
 
+    skl = GET_CHAR_SKILL(tch, SKILL_DISRUPTIVE_BLOW);
     success = skl - number(0, 130);
 
     if (skl && success)
     {
       notch_skill(ch, SKILL_DISRUPTIVE_BLOW, 5);
-      
+
       if (success > 75)
       {
         if (melee_damage(tch, ch, 4 * (dice(5, 10)), 0, &messages) == DAM_NONEDEAD)
