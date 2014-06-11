83743caf1f75456c5efdf28ead80fde51f7008d8
Lohrr <Lohrr2k@yahoo.com>
2014-06-11 01:48:39 +0000

    Identify / legend lore now shows item value for the item.

--- src/magic.c
+++ src/magic.c
@@ -10266,6 +10266,8 @@ void spell_identify(int level, P_char ch, char *arg, int type, P_char victim,
         send_to_char(Gbuf1, ch);
       }
     }
+    sprintf( Gbuf1, "$p &nhas an item value of &+W%d&n.", itemvalue(ch, obj) );
+    act( Gbuf1, FALSE, ch, obj, 0, TO_CHAR );
   }
   else
   {
