a4c7fd89156d8913a3b7d38eb74c3537b3b730eb
Lohrr <Lohrr2k@yahoo.com>
2014-07-23 04:04:42 +0000

    Re-added ship frags being necessary for capitol weapons on ships.

--- src/ships/ship_shop.c
+++ src/ships/ship_shop.c
@@ -1581,13 +1581,15 @@ int buy_weapon(P_char ch, P_ship ship, char* arg1, char* arg2)
         send_to_char ("That weapon cannot be placed in that position, try another one.\r\n", ch);
         return TRUE;
     }
+
 /* -Removing frag requirements for weapons - Drannak 6/3/2013
+ *  Readding them. - Lohrr 7/23/2014
+*/
     if (ship->frags < weapon_data[w].min_frags)
     {
         send_to_char ("I'm sorry, but not just anyone can buy this weapon!  You must earn it!\r\n", ch);
         return TRUE;
     }
-*/
 
     if (IS_SET(weapon_data[w].flags, CAPITOL)) 
     {
